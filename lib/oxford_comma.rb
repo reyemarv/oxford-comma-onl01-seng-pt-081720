f75bc877e535178a217600e54bbf562351f3b4e1
Marvin Reyes <reyemarv@gmail.com>
2020-11-23 03:35:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2 
     array.join(" and ")
   elsif array.length > 2 
-    array.insert(-1, "and ")
+    array.insert(-2, "and ")
   else array[0]
   end 
 end
