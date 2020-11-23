e5dc3891496e4aa36f824ad74cdf0a23c065b6ac
Marvin Reyes <reyemarv@gmail.com>
2020-11-23 03:38:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.length == 2 
     array.join(" and ")
   elsif array.length > 2 
-    array.insert(-2, "and ")
     array.join (", ")
+    array.insert(-2, "and ")
+    # array.join (", ")
   else array[0]
   end 
 end
